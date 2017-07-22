37750991b44c6d3f2e5139b6f98c01633739c5ae
insanitybit <insanitybit@gmail.com>
2017-07-22 16:47:34 -0400

    Start testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+
+
 #![allow(dead_code)]
 
 extern crate futures_cpupool;
@@ -11,7 +13,7 @@ use tokio_core::reactor::{Core, Timeout, Handle};
 use tokio_timer::Timer;
 
 use futures_cpupool::CpuPool;
-use futures::{Future, Stream};
+use futures::{Future, Stream, IntoFuture};
 use futures::future::{Executor, Loop, Either, loop_fn, empty, select_all};
 
 use futures_mpsc::{UnboundedSender, UnboundedReceiver, unbounded};
@@ -87,65 +89,66 @@ impl ScheduledExecutorService {
                      mut receiver: UnboundedReceiver<ExecutorMessage>,
                      mut timers: HashMap<String, (Box<Future<Item=(), Error=()>>, u64, Box<Fn() + Send + 'static>)>) {
         let handle = handle.clone();
-
+        let h = handle.clone();
         let timer_future = select_all(timers.drain().map(|(id, (fut, it, then))| {
             fut.map(move |f| (id, f, it, then))
         }));
 
-        // We want to either handle the case where we receive a new message or where one of our timers
-        // has expired
-        //
-        // In the case of a new message we want to handle either cancelling an old future by ID or
-        // we want to add the new timer to the timers map
-        //
-        // In the case of our timer expiring we want to reinsert the old timers back into the map
-        // and then call the function associated with the timer
-        receiver.into_future().select2(timer_future).then(move |res| {
-            match res {
-                Ok(o) => {
-                    match o {
-                        // Received a new message
-                        Either::A((receive_future, timer_future)) => {
+        let main_loop = loop_fn((receiver.into_future(), timer_future, timers, handle), move |res| {
 
-                            let msg = receive_future.0;
+            let (receiver, timer_future, mut timers, handle) = res;
+            // We want to either handle the case where we receive a new message or where one of our timers
+            // has expired
+            //
+            // In the case of a new message we want to handle either cancelling an old future by ID or
+            // we want to add the new timer to the timers map
+            //
+            // In the case of our timer expiring we want to reinsert the old timers back into the map
+            // and then call the function associated with the timer
+            receiver.into_future().select2(timer_future).then(|res| {
+                match res {
+                    Ok(o) => {
+                        match o {
+                            // Received a new message
+                            Either::A((receive_future, timer_future)) => {
 
-                            if let Some(msg) = msg {
-                                match msg {
-                                    ExecutorMessage::ExecuteAfter {id, timeout, time, f} => {
-                                        execute_after(&mut timers, id, timeout, time, f)
-                                    },
-                                    ExecutorMessage::Cancel {id} => {
-                                        let timer = timers.remove(&id).unwrap();
-                                        drop(timer)
+                                let msg = receive_future.0;
+
+                                if let Some(msg) = msg {
+                                    match msg {
+                                        ExecutorMessage::ExecuteAfter {id, timeout, time, f} => {
+                                            execute_after(&mut timers, id, timeout, time, f)
+                                        },
+                                        ExecutorMessage::Cancel {id} => {
+                                            let timer = timers.remove(&id).unwrap();
+                                            drop(timer)
+                                        }
                                     }
                                 }
+
+                                Ok(Loop::Continue((receive_future.1.into_future(), timer_future, timers, handle)))
+                            },
+                            // Timer has fired
+                            Either::B(((timer_result, _, timer_futures), receive_future)) => {
+                                let (id, _, time, f) = timer_result;
+                                f();
+
+                                let time = time * 2;
+                                let timeout = Timeout::new(Duration::from_secs(time), &handle).unwrap();
+                                execute_after(&mut timers, id, timeout, time, f);
+
+                                Ok(Loop::Continue((receive_future, select_all(timer_futures), timers, handle)))
                             }
-
-                            Ok((receive_future.1.into_future(), timer_future))
-                        },
-                        // Timer has fired
-                        Either::B(((timer_result, _, timer_futures), receive_future)) => {
-                            let (id, _, time, f) = timer_result;
-                            f();
-
-                            let time = time * 2;
-                            let timeout = Timeout::new(Duration::from_secs(time), &handle).unwrap();
-                            execute_after(&mut timers, id, timeout, time, f);
-
-                            Ok((receive_future, select_all(timer_futures)))
                         }
+                    },
+                    Err(e) => {
+                        Ok(Loop::Break(()))
                     }
-                },
-                Err(e) => {
-                    Err(())
                 }
-            };
-            if true {
-                Ok(())
-            } else {
-                Err(())
-            }
+            })
         });
+
+        h.execute(main_loop).unwrap();
     }
 }
 
@@ -161,7 +164,23 @@ pub fn execute_after(timers: &mut HashMap<String, (Box<Future<Item=(), Error=()>
 
 #[cfg(test)]
 mod tests {
+
+    use super::*;
+
     #[test]
     fn it_works() {
+
+        let mut executor = ScheduledExecutor::new();
+        let core = Core::new().unwrap();
+        let handle = core.handle();
+
+        let timeout = Timeout::new(Duration::from_secs(1), &handle).unwrap();
+
+        executor.execute_after(timeout, 1,
+        Box::new(|| {
+            println!("foo");
+        }));
+
+        thread::sleep(Duration::from_secs(30));
     }
 }
